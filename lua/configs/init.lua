e0ab9e5e8a656e3c5e0f4153e4a19ab0e68a8d0a
ArcheryLuna <76516425+ArcheryLuna@users.noreply.github.com>
2024-03-10 06:04:40 +0000

    Initial Commit

---
+++ lua/configs/init.lua
@@ -0,0 +1,25 @@
+-- Import required mappings
+require("configs/mappings")
+require("configs/options")
+
+-- Color Scheme Settings
+vim.cmd.colorscheme("tokyonight")
+vim.api.nvim_set_hl(0, "Normal", {bg="none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
+
+-- Better File Tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+require("nvim-tree").setup({
+	view = {
+		width = 25,
+	},
+	filters = {
+		dotfiles = true,
+	},
+})
+
+vim.cmd[[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
+
---
+++ lua/plugins/init.lua
@@ -0,0 +1,64 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  	-- Packer can manage itself
+  	use {'wbthomason/packer.nvim'}
+
+	use {
+  		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                          , branch = '0.1.x',
+  		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {
+  		"folke/tokyonight.nvim",
+	}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		{run = ':TSUpdate'}
+	}
+	use {
+		'nvim-treesitter/playground'
+	}
+	use {
+		'theprimeagen/harpoon'
+	}
+	use {
+		'mbbill/undotree'
+	}
+	use {
+		'tpope/vim-fugitive'
+	}
+
+	-- Better file manager
+	use {
+		'nvim-tree/nvim-tree.lua'
+	}
+
+	-- Tmux Naviagation
+	use {
+		'christoomey/vim-tmux-navigator'
+	}
+
+	-- LSP Zero
+	use {
+  		'VonHeikemen/lsp-zero.nvim',
+  		branch = 'v3.x',
+  		requires = {
+   		 	--- Uncomment the two plugins below if you want to manage the language servers from neovim
+    			{'williamboman/mason.nvim'},
+    			{'williamboman/mason-lspconfig.nvim'},
+
+    			-- LSP Support
+    			{'neovim/nvim-lspconfig'},
+    			-- Autocompletion
+    			{'hrsh7th/nvim-cmp'},
+    			{'hrsh7th/cmp-nvim-lsp'},
+    			{'L3MON4D3/LuaSnip'},
+  		}
+	}
+
+    -- Github Copilot When on new PC use the `:copilot setup` command 
+    use {
+        'github/copilot.vim'
+    }
+end)
